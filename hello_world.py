b437c3fff502dee872040c635773d5bb650c4f0d
S.D.N Chanaka Fernando <nuwan.c.fernando@gmail.com>
2017-12-17 11:47:06 +0530

    (#001) - The input(<Prompt Msg>) and print(<whatever>) functions

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+# 1. notify user to enter a message
+print("Hello, Bro please enter your details")
+
+# 2. Catch the user input
+fname = input("First name : ")
+lname = input("Last name : ")
+age = input("Age : ")
+
+# 3. Display the processed message
+print("=============================")
+print("First name : ", fname, '\n'
+      "Last name : ", lname, '\n'
+      "Age : ", age)
+print("=============================")
